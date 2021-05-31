e80c53d0b3cfa7f481468a27acb9a280770294ca
apophthegm <didimar2001@gmail.com>
2021-05-31 14:13:32 -0500

    navBar is done, now, about me section is unfinished

---
+++ index.js
@@ -0,0 +1,18 @@
+// ABOUT ME ANIMATION
+
+const animations = (elementId) => {
+  let animationEffect = document.getElementById(elementId);
+  let scrollPosition = animationEffect.getBoundingClientRect().top;
+  let screenSize = window.innerHeight;
+
+  if (scrollPosition < screenSize - 100) {
+    animationEffect.style.animation = "showAnimation 1s none";
+    animationEffect.style.opacity = 1;
+  } else {
+    animationEffect.style.transition = "all .3s ease";
+    animationEffect.style.opacity = 0;
+    animationEffect.style.animation = "";
+  }
+};
+
+window.addEventListener("scroll", () => animations("animation"));
