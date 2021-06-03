5ae03c7d9dc435f165f74ddcb3e703418287259e
apophthegm <didimar2001@gmail.com>
2021-06-02 22:10:37 -0500

    Skills page is done! :D and now all my pages are responsive :D

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 // ABOUT ME ANIMATION
-
 const animations = (elementId) => {
   let animationEffect = document.getElementById(elementId);
   let scrollPosition = animationEffect.getBoundingClientRect().top;
@@ -14,6 +13,68 @@ const animations = (elementId) => {
     animationEffect.style.animation = "";
   }
 };
-
 window.addEventListener("scroll", () => animations("animation"));
 window.addEventListener("scroll", () => animations("about-me-footer"));
+
+// PROJECTS ANIMATION
+const animationTwo = (elementId) => {
+  let animationEffect = document.getElementById(elementId);
+  let scrollPosition = animationEffect.getBoundingClientRect().top;
+  let screenSize = window.innerHeight;
+
+  if (scrollPosition < screenSize - 100) {
+    animationEffect.style.animation = "animation2 1s none";
+    animationEffect.style.opacity = 1;
+  } else {
+    animationEffect.style.transition = "all .3s ease";
+    animationEffect.style.opacity = 0;
+    animationEffect.style.animation = "";
+  }
+};
+
+const animationThree = (elementId) => {
+  let animationEffect = document.getElementById(elementId);
+  let scrollPosition = animationEffect.getBoundingClientRect().top;
+  let screenSize = window.innerHeight;
+
+  if (scrollPosition < screenSize - 100) {
+    animationEffect.style.animation = "animation3 1s .6s none";
+    animationEffect.style.animationFillMode = "both";
+    animationEffect.style.opacity = 1;
+  } else {
+    animationEffect.style.transition = "all .3s ease";
+    animationEffect.style.opacity = 0;
+    animationEffect.style.animation = "";
+  }
+};
+window.addEventListener("scroll", () => animationTwo("projects-text"));
+window.addEventListener("scroll", () => animationThree("projects-list"));
+
+// SKILLS AIMATION
+const animationFour = (elementId) => {
+  let animationEffect = document.getElementById(elementId);
+  let scrollPosition = animationEffect.getBoundingClientRect().top;
+  let screenSize = window.innerHeight;
+
+  if (scrollPosition < screenSize - 100) {
+    animationEffect.style.animation = "animation4 1s none";
+    animationEffect.style.opacity = 1;
+  } else {
+    animationEffect.style.transition = "all .3s ease";
+    animationEffect.style.opacity = 0;
+    animationEffect.style.animation = "";
+  }
+};
+window.addEventListener("scroll", () => animationFour("skills-text"));
+window.addEventListener("scroll", () => animationTwo("languages"));
+
+// MENU ICON TOGGLE
+window.onscroll = () => {
+  let scroll = document.documentElement.scrollTop;
+  if (scroll > 400) {
+    document.getElementById("show-menu-icon").style.transform = "scale(1)";
+  } else {
+    document.getElementById("menu-checkbox").checked = false;
+    document.getElementById("show-menu-icon").style.transform = "scale(0)";
+  }
+};
