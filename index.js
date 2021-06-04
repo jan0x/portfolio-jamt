795a9d8068c023ddcf7080c642b68e8e7d4eec33
apophthegm <didimar2001@gmail.com>
2021-06-04 14:23:36 -0500

    fixing some bugs on contact page

--- index.js
+++ index.js
@@ -86,6 +86,8 @@ const contactAnimation = () => {
 window.addEventListener("scroll", () => contactAnimation());
 
 // MENU ICON TOGGLE
+window.onload = () =>
+  (document.getElementById("show-menu-icon").style.transform = "scale(0)");
 window.onscroll = () => {
   let scroll = document.documentElement.scrollTop;
   if (scroll > 400) {
