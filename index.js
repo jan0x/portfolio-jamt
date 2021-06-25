f0ca7be08421c8cdca0625e11b76cea18c62fb8c
apophthegm <didimar2001@gmail.com>
2021-06-25 01:09:25 -0500

    Fixing scroll

--- index.js
+++ index.js
@@ -105,4 +105,5 @@ window.onload = function () {
   document.getElementById("loading").style.opacity = "0";
   document.getElementById("loading").style.visibility = "hidden";
   document.body.style.overflowY = "auto";
+  window.scrollTo(0, 0);
 };
